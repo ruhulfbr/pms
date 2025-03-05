427bb4438543688974230bef7564c8311d861d54
Md Ruhul Amin <ruhul11bd@gmail.com>
2025-03-05 13:00:11 +0600

    Making application structure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,12 +7,17 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
+        api: [
+            __DIR__.'/../routes/api.php',
+            __DIR__.'/../routes/auth.php',
+        ],
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api(prepend: [
+            \App\Http\Middleware\ForceJsonResponse::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
